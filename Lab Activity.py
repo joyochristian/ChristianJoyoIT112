d8d2a45e9325572e67ed6c130270dcdd754cf9df
joyochristian <joyoela@gmail.com>
2026-02-13 00:36:11 +0800

    Add files via upload

---
+++ Lab Activity.py
@@ -0,0 +1,26 @@
+name = "Christian Jhon S. Joyo"
+age = 19
+height = 1.69   # float (decimal number)
+course_section = "BSIT - IT1R8"
+has_work = False
+
+print("===== PERSONAL INFORMATION =====\n")
+
+print(f"Fullname: {name}")
+print("Data type:", type(name))
+print()
+
+print(f"Age: {age}")
+print("Data type:", type(age))
+print()
+
+print(f"Height: {height} meters")
+print("Data type:", type(height))
+print()
+
+print(f"Course & Section: {course_section}")
+print("Data type:", type(course_section))
+print()
+
+print(f"Currently Working?: {has_work}")
+print("Data type:", type(has_work))
